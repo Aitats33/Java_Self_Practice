b13df3d125f5af6486a68ccb25f167ddbf526944
aytac <aytacyankia@gmail.com>
2024-02-11 22:36:00 +0000

    Item

---
+++ src/day12_practice_task/ItemClass.java
@@ -0,0 +1,18 @@
+package day12_practice_task;
+
+public class ItemClass {
+
+    public static void main(String[] args) {
+
+        Item item1 = new Item("Car", 17_500, 3);
+
+        Item item2 = new Item("house", 500_000, 4);
+
+
+        System.out.println(item1);
+        System.out.println(item2);
+
+
+    }
+
+}
